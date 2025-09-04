9e93e2644bcb35f55fd90f6b454191c45d6ef179
ClumsyLulz <86472964+SleepTheGod@users.noreply.github.com>
2025-09-04 06:07:17 -0400

    Update dscan.sh

--- dscan.sh
+++ dscan.sh
@@ -54,78 +54,43 @@ if [ -z "${TARGET_URL-}" ]; then
 fi
 
 # ------------------------------
-# Paths to check (full list)
+# Paths to check
 # ------------------------------
-paths_to_check=( 
-"core/install.php" "core/authorize.php" "core/rebuild.php"
-"core/modules/statistics/statistics.php" "core/modules/system/tests/https.php"
-"core/modules/system/tests/http.php" "autoload.php" "composer.json" "composer.lock"
-".git" ".svn" ".DS_Store" ".well-known" "CHANGELOG.txt" "INSTALL.txt"
-"LICENSE.txt" "MAINTAINERS.txt" "README.txt" "UPGRADE.txt" "phpinfo.php"
-".htaccess" "robots.txt" "web.config" ".env" ".htpasswd" "includes"
-"misc" "modules" "profiles" "scripts" "sites" "themes"
-"/admin" "/admin/config" "/admin/config/system" "/admin/config/people"
-"/admin/config/media" "/admin/appearance" "/admin/modules" "/admin/content"
-"/admin/reports" "/admin/structure" "/admin/structure/block" "/admin/structure/taxonomy"
-"/admin/structure/views" "/admin/structure/menu" "/admin/structure/paragraphs"
-"/admin/structure/layout" "/admin/structure/search" "/admin/structure/entity"
-"/admin/structure/migrate" "/admin/structure/fields" "/admin/structure/users"
-"/admin/structure/custom-blocks" "/admin/config/services" "/admin/config/media/image-style"
-"/admin/config/system/performance" "/admin/config/system/smtp"
-"/admin/config/search/search-api" "/admin/config/search/search-api/index"
-"/admin/config/search/search-api/server" "/admin/config/development/logging"
-"/admin/config/development/cache" "/admin/config/development/performance"
-"/admin/config/development/debugging" "/admin/config/development/redis"
-"/admin/config/development/override" "/admin/config/development/agentrace"
-"/admin/config/people/accounts" "/admin/config/people/password-policy"
-"/admin/config/people/roles" "/admin/config/people/permissions"
-"/admin/config/people/registration" "/admin/config/people/session"
-"/admin/config/people/login" "/admin/config/people/accounts/form"
-"/admin/config/people/roles/permissions" "/admin/config/people/roles/create"
-"/admin/config/people/roles/update" "/admin/config/people/roles/delete"
-"/admin/content/{content_type}" "admin/content/{content_type}/add"
-"/admin/content/{content_type}/edit/{node_id}" "admin/content/{content_type}/delete/{node_id}"
-"/node/add" "/node/add/article" "/node/add/page" "/node/add/story"
-"/node/{nid}/edit" "/node/{nid}/delete" "/node/{nid}/view"
-"/user/login" "/user/logout" "/user/register" "/user/password"
-"/user/{uid}" "/user/{uid}/edit" "/user/{uid}/delete" "/user/{uid}/roles"
-"/user/{uid}/access" "/user/{uid}/content" "/user/{uid}/settings"
-"/user/{uid}/session" "/user/{uid}/password" "/user/{uid}/profile"
-"/user/{uid}/subscriptions" "/user/{uid}/posts" "/user/{uid}/comments"
-"/user/{uid}/notifications" "/user/{uid}/messages" "/user/{uid}/inbox"
-"/user/{uid}/outbox" "/user/{uid}/activity"
-"/core" "/core/install.php" "/core/misc" "/core/scripts" "/core/vendor"
-"/core/lib" "/core/themes" "/core/assets" "/sites/default/files/"
-"/sites/default/private/" "/sites/default/settings.php" "/sites/default/cron.php"
-"/rest/session/token" "/rest/views/{view_name}/page" "/rest/views/{view_name}/json"
-"/rest/views/{view_name}/rss" "/rest/views/{view_name}/xml" "/rest/{resource_name}"
-"/rest/{resource_name}/{id}" "/entity/{entity_type}/{id}" "/entity/{entity_type}/{id}/edit"
-"/entity/{entity_type}/{id}/delete" "/entity/{entity_type}/{id}/view"
-"/entity/{entity_type}/{id}/field" "/entity/{entity_type}/{id}/permissions"
-"/entity/{entity_type}/{id}/assign" "/entity/{entity_type}/{id}/parent"
-"/entity/{entity_type}/{id}/content" "/entity/{entity_type}/{id}/custom-fields"
-"/entity/{entity_type}/create" "/entity/{entity_type}/update" "/entity/{entity_type}/delete"
-"/entity/{entity_type}/views" "/entity/{entity_type}/manage" "/entity/{entity_type}/settings"
-"/entity/{entity_type}/rules" "/entity/{entity_type}/permissions" "/entity/{entity_type}/translations"
-"/entity/{entity_type}/variants" "/entity/{entity_type}/taxonomy" "/entity/{entity_type}/comments"
-"/entity/{entity_type}/comment-form" "/entity/{entity_type}/fields" "/entity/{entity_type}/view-form"
-"/entity/{entity_type}/create-form" "/entity/{entity_type}/edit-form"
-"/entity/{entity_type}/translations-form" "/entity/{entity_type}/delete-form"
-"/entity/{entity_type}/field/{field_name}" "/entity/{entity_type}/field/{field_name}/add"
-"/entity/{entity_type}/field/{field_name}/edit" "/entity/{entity_type}/field/{field_name}/delete"
-"/entity/{entity_type}/field/{field_name}/view" "/entity/{entity_type}/field/{field_name}/settings"
-"/entity/{entity_type}/field/{field_name}/permissions" "/entity/{entity_type}/field/{field_name}/value"
-"/entity/{entity_type}/field/{field_name}/translations" "/entity/{entity_type}/field/{field_name}/text"
-"/entity/{entity_type}/field/{field_name}/field-type" "/entity/{entity_type}/field/{field_name}/create"
-"/entity/{entity_type}/field/{field_name}/update" "/entity/{entity_type}/field/{field_name}/remove"
-"/entity/{entity_type}/field/{field_name}/delete-form" "/entity/{entity_type}/field/{field_name}/add-field"
-"/entity/{entity_type}/field/{field_name}/field-edit" "/entity/{entity_type}/field/{field_name}/edit-form"
-"/entity/{entity_type}/field/{field_name}/update-form" "/entity/{entity_type}/field/{field_name}/translations-form"
-"/entity/{entity_type}/field/{field_name}/view-form" "/entity/{entity_type}/field/{field_name}/field-definition"
-"/entity/{entity_type}/field/{field_name}/create-form" "/entity/{entity_type}/field/{field_name}/remove-form"
-"/entity/{entity_type}/field/{field_name}/field-definition-form" "/entity/{entity_type}/field/{field_name}/permissions-form"
-"/robots.txt" "/crossdomain.xml" "/xmlrpc.php" "/update.php" "/about" "/help"
-"/donate" "/terms-of-service" "/privacy-policy" "/404"
+paths_to_check=(
+/admin /admin/config /admin/config/system /admin/config/people /admin/config/media
+/admin/appearance /admin/modules /admin/content /admin/reports /admin/structure
+/admin/structure/block /admin/structure/taxonomy /admin/structure/views
+/admin/structure/menu /admin/structure/paragraphs /admin/structure/layout
+/admin/structure/search /admin/structure/entity /admin/structure/migrate
+/admin/structure/fields /admin/structure/users /admin/structure/custom-blocks
+/admin/config/services /admin/config/media/image-style /admin/config/system/performance
+/admin/config/system/smtp /admin/config/search/search-api
+/admin/config/search/search-api/index /admin/config/search/search-api/server
+/admin/config/development/logging /admin/config/development/cache
+/admin/config/development/performance /admin/config/development/debugging
+/admin/config/development/redis /admin/config/development/override
+/admin/config/development/agentrace /admin/config/people/accounts
+/admin/config/people/password-policy /admin/config/people/roles
+/admin/config/people/permissions /admin/config/people/registration
+/admin/config/people/session /admin/config/people/login
+/admin/config/people/accounts/form /admin/config/people/roles/permissions
+/admin/config/people/roles/create /admin/config/people/roles/update
+/admin/config/people/roles/delete /admin/content/{content_type}
+/admin/content/{content_type}/add /admin/content/{content_type}/edit/{node_id}
+/admin/content/{content_type}/delete/{node_id} /node/add /node/add/article
+/node/add/page /node/add/story /node/{nid}/edit /node/{nid}/delete /node/{nid}/view
+/user/login /user/logout /user/register /user/password /user/{uid} /user/{uid}/edit
+/user/{uid}/delete /user/{uid}/roles /user/{uid}/access /user/{uid}/content
+/user/{uid}/settings /user/{uid}/session /user/{uid}/password /user/{uid}/profile
+/user/{uid}/subscriptions /user/{uid}/posts /user/{uid}/comments /user/{uid}/notifications
+/user/{uid}/messages /user/{uid}/inbox /user/{uid}/outbox /user/{uid}/activity
+/core /core/install.php /core/misc /core/scripts /core/vendor /core/lib /core/themes
+/core/assets /sites/default/files/ /sites/default/private/ /sites/default/settings.php
+/sites/default/cron.php /rest/session/token /rest/views/{view_name}/page
+/rest/views/{view_name}/json /rest/views/{view_name}/rss /rest/views/{view_name}/xml
+/rest/{resource_name} /rest/{resource_name}/{id} /entity/{entity_type}/{id}
+/robots.txt /crossdomain.xml /xmlrpc.php /update.php /about /help /donate
+/terms-of-service /privacy-policy /404
 )
 
 # ------------------------------
@@ -153,6 +118,9 @@ check_path() {
         echo "[FOUND] $url/$path (status $status)"
     fi
 }
+
+export -f check_path
+export TARGET_URL
 
 scan_security_headers() {
     local url="$1"
@@ -188,7 +156,7 @@ echo "[*] Checking security headers..."
 scan_security_headers "$TARGET_URL"
 
 echo "[*] Enumerating paths..."
-printf "%s\n" "${paths_to_check[@]}" | xargs -n1 -P "$MAX_THREADS" -I{} bash -c 'check_path "'"$TARGET_URL"'" "{}"'
+printf "%s\n" "${paths_to_check[@]}" | xargs -n1 -P "$MAX_THREADS" -I{} bash -c 'check_path "$0" "$1"' "$TARGET_URL" {}
 
 echo "[*] Checking known Drupal vulnerabilities..."
 scan_drupal_vulns "$TARGET_URL"
